7028f81e62893d6507081f269fcbadd383277656
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2020-10-27 02:26:06 -0600

    La de pruebas 2 sirve

---
+++ Assembly/PRUEBAS2-ETCH.X/build/default/production/PRUEBAS2.o.d
@@ -0,0 +1,3 @@
+build/default/production/PRUEBAS2.o:  \
+	C:/Users/noelp/Documents/GitHub/Proyecto\ 2/Assembly/PRUEBAS2-ETCH.X/PRUEBAS2.asm  \
+
---
+++ Assembly/Prueba1millon.X/build/default/production/PRB.o.d
@@ -0,0 +1,3 @@
+build/default/production/PRB.o:  \
+	C:/Users/noelp/Documents/GitHub/Proyecto\ 2/Assembly/Prueba1millon.X/PRB.asm  \
+
